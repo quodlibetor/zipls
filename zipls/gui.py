77e7ff08debe890f263936d1f9c8460015963c6a
bwmaister <bwmaister@gmail.com>
2011-01-22 17:58:04 -0500

    Improved gui, and the ability to change the target zip file

--- zipls/gui.py
+++ zipls/gui.py
@@ -8,9 +8,12 @@ import zipls
 class Playlists(Tk.Frame):
     def __init__(self, master, playlists=None):
         Tk.Frame.__init__(self, master, padx=6)
+        self.playlist_frames = Tk.Frame(self)
+        self.playlist_frames.pack()
+
         self.add_button = Tk.Button(self, text="Add another playlist",
                                     command=self.add)
-        self.add_button.pack()
+        self.add_button.pack(pady=3)
         if playlists is None:
             self.add()
         else:
@@ -19,12 +22,12 @@ class Playlists(Tk.Frame):
         self.pack()
 
     def add(self, pls=None):
-        new_pls = PlaylistBox(self)
+        new_pls = PlaylistBox(self.playlist_frames)
         if pls is not None:
             new_pls.filepath = pls
             new_pls.label_text.set(pls)
         new_pls.pack()
-        self.pack()
+        self.playlist_frames.pack()
 
 
 class PlaylistBox(Tk.Frame):
@@ -41,8 +44,9 @@ class PlaylistBox(Tk.Frame):
                           bd=1, relief=Tk.SUNKEN)
         self.filepath = None
 
+        self.empty_text = "No playlist selected"
         self.label_text = Tk.StringVar()
-        self.label_text.set("No playlist selected")
+        self.label_text.set(self.empty_text)
         self.label = Tk.Label(self, textvariable=self.label_text)
         self.label.pack()
 
@@ -60,20 +64,81 @@ class PlaylistBox(Tk.Frame):
 
     def get_file(self):
         filepath = tkFileDialog.askopenfilename(filetypes=PlaylistBox.filetypes)
-        if filepath is not None:
+        if filepath:
             self.label_text.set(filepath)
             self.filepath = filepath
 
+            if PlaylistBox.playlists[0] is self and \
+                    TargetBox.single.target is None:
+                target = os.path.splitext(filepath)[0]
+                target += '.zip'
+                TargetBox.single.change_to(target)
+
+        elif self.filepath is not None:
+            self.label_text.set(self.filepath)
+        else:
+            self.label_text.set(self.empty_text)
+
+        self.label.pack()
+
     def clear(self):
-        self.label_text.set("No playlist selected")
+        self.label_text.set(self.empty_text)
         self.filepath = None
 
-class Controls(Tk.Frame):
+class TargetBox(Tk.Frame):
+    # pseudo-singletonny object access
+    single = None
+
     def __init__(self, master):
+        if TargetBox.single is not None:
+            raise RuntimeError("there can only be one target.")
+        TargetBox.single = self
+
+        Tk.Frame.__init__(self, master,
+                          padx=3, bd=1, relief=Tk.SUNKEN)
+
+        self.change_button = Tk.Button(self, text="Change",
+                                       command=self.choose_other)
+
+        self.target = None
+        self.fmt = 'Target: {0}'
+        self.empty_text = "(Nothing selected)"
+        self.label_text = Tk.StringVar()
+        self.label_text.set(self.fmt.format(self.empty_text))
+        self.label = Tk.Label(self, textvariable=self.label_text)
+
+        self.label.pack(side=Tk.LEFT)
+        self.change_button.pack(side=Tk.RIGHT)
+        self.pack(side=Tk.TOP, padx=2, pady=5)
+
+
+    def choose_other(self):
+        filepath = tkFileDialog.asksaveasfilename(filetypes=(("Zip file", "*.zip"),
+                                                             ("Anything", "*")))
+        dirname = os.path.dirname(filepath)
+        if not os.path.isdir(dirname) and dirname != '':
+            answer = tkMessageBox.askyesno("Directory does not exist",
+                                           "The directory '{0}' doesn't exist.\n"
+                                           "Do you want to create it?".format(dirname))
+            if answer == 'yes':
+                os.makedirs(dirname)
+        if filepath:
+            self.change_to(filepath)
+
+    def change_to(self, filepath):
+        TargetBox.target = self.target = filepath
+        self.label_text.set(self.fmt.format(filepath))
+        self.label.pack()
+
+class Controls(Tk.Frame):
+    def __init__(self, master, Control_Frames):
         Tk.Frame.__init__(self, master)
 
         self.zip_button = Tk.Button(self, text="Zip!",
                                     command=self.zip)
+
+        for Frame in Control_Frames:
+            Frame(self)
         self.zip_button.pack()
         self.pack()
 
@@ -81,11 +146,12 @@ class Controls(Tk.Frame):
         plss = [pls.filepath
                 for pls in PlaylistBox.playlists if pls.filepath]
         if not plss:
-            tkMessageBox("No playlists selected!")
+            tkMessageBox.showwarning("Eek!", "No playlists selected!")
             return
 
-        target = os.path.splitext(plss[0])[0]
-        target += '.zip'
+        target = TargetBox.single.target
+        if not target.endswith('.zip'):
+            target = os.path.splitext(target)[0] + '.zip'
         songs = zipls.Songs(plss)
         songs.zip_em(target)
 
@@ -100,14 +166,15 @@ def main(args):
     root = Tk.Tk()
     root.title("Zip, Please!")
 
-    w = Tk.Label(root, text="Welcome to zipls!")
+    w = Tk.Label(root, text="Welcome to zipls!",
+                 pady=7)
     w.pack()
 
     if args.playlist:
         Playlists(root, args.playlist)
     else:
         Playlists(root)
-    Controls(root)
+    Controls(root, [TargetBox])
 
     root.mainloop()
 
